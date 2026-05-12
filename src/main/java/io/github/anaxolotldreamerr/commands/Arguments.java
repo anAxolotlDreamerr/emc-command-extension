86a4dce4d66ac5d4e75417940cab2bc9025ba9a3
AxolotlDreamer <anAxolotlDreamer@gmail.com>
2026-05-12 23:45:48 +0800

    Initial commit

---
+++ src/main/java/io/github/anaxolotldreamerr/commands/Arguments.java
@@ -0,0 +1,25 @@
+package io.github.anaxolotldreamerr.commands;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public enum Arguments {
+    ID("-i"),TOWN("-t"),NATION("-n"),PLAYER("-p"),RESIDENT("-r");
+    private static final Map<String,Arguments> LOOKUP = new HashMap<>();
+    private final String ARG;
+    Arguments(String arg){
+        this.ARG = arg;
+        put(arg,this);
+    }
+    private static void put(String sArg,Arguments arg){
+        if(LOOKUP.containsKey(sArg))throw new IllegalArgumentException("Duplicate enum field name:"+arg+"-"+sArg);
+        LOOKUP.put(sArg,arg);
+    }
+    public static Set<Arguments> match(String... args){
+        return Arrays.stream(args).filter(arg ->
+                for(int sArg :args){
+                    if(LOOKUP.containsKey(arg))return true;
+        }
+                )
+    }
+}
--- src/main/java/io/github/anaxolotldreamerr/commands/CommandRegistry.java
+++ src/main/java/io/github/anaxolotldreamerr/commands/CommandRegistry.java
@@ -1,15 +1,19 @@
 package io.github.anaxolotldreamerr.commands;
+import com.mojang.brigadier.RedirectModifier;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.tree.ArgumentCommandNode;
+import com.mojang.brigadier.tree.CommandNode;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 
 public class CommandRegistry {
     public static void test(){
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            dispatcher.register(Commands.literal("test_command").executes(context -> {
-                context.getSource().sendSuccess(() -> Component.literal("Called /test_command."), false);
-                return 1;
-            }));
+            dispatcher.register(Commands.literal("favourites")
+                    .then(Commands.argument("args", StringArgumentType.word())))
         });
     }
 }
